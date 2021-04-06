c60795ff1ac74091fde2d35fce48eebb0499d39e
Christian Sparks <csparks1919@hotmail.com>
2021-04-06 13:19:31 -0500

    add fake api route

--- flask-server/server.py
+++ flask-server/server.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 @app.route('/', methods=['GET'])
 def serve_dir_directory_index():
     return render_template("index.html", flask_token="Hello   world")
- 
+
+@app.route('/apitest', methods=['GET'])
+def test_api_route():
+    return "This is a test"
+
 app.run(host='0.0.0.0',port=8080)
