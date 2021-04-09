a1b3e0181050f05315cf6249cfd2ddbb85056a14
AT1452 <athosar1452@gmail.com>
2021-04-09 10:41:19 -0500

    testing db miport

---
+++ flask-server/api/restaurant.py
@@ -0,0 +1,53 @@
+from __main__ import (app)
+import db
+from flask import (jsonify, request)
+
+# Get all of a user's order preferences
+@app.route('/findRestaurant/<restaurant_id>', methods=['GET'])
+def findRestaurant(restaurant_id):
+    conn, mycursor = db.db_conn()
+    query = """SELECT r.restaurant_name, r.location, r.rating, r.website, r.food_types
+               FROM Restaurant r
+                WHERE r.restaurant_id = %s"""
+    params = (str(restaurant_id), )
+    mycursor.execute(query, params)
+
+    restaurant = []
+    for (restaurant_name, location, rating, website, food_types) in cursor:
+        restaurant.append({
+            'location': location,
+            'rating': rating,
+            'website': website,
+            'restaurant_name': restaurant_name,
+            'food_types': food_types
+        })
+    
+    mycursor.close()
+    conn.close()
+    return jsonify({'restaurant':restaurant})
+
+@app.route('/newRestaurant', methods=['POST'])
+def createRestaurant():
+    conn, mycursor = db.db_conn()
+    print("hellooo")
+    data = request.json;
+    print(data)
+    query = """INSERT INTO Restaurant(restaurant_id, restaurant_name, location, rating, website, food_types)
+               VALUES(%s, %s, %s, %s, %s, %s) """
+    params = (str(data['crid']), str(data['rname']), str(data['rloc']),
+                str(data['rrate']), str(data['rsite']), str(data['rcuis']))
+    print(query);
+    mycursor.execute(query, params)
+
+    # restaurant = []
+    # for (restaurant_name, location, rating, website, food_types) in mycursor:
+    #     restaurant.append({
+    #         'location': location,
+    #         'rating': rating,
+    #         'website': website,
+    #         'restaurant_name': restaurant_name,
+    #         'food_types': food_types
+    #     })
+    # return jsonify({'restaurant':restaurant})
+    print("done")
+    return "Done!!"
--- flask-server/app.py
+++ flask-server/app.py
@@ -1,6 +1,5 @@
-from flask import (Flask, render_template, jsonify)
+from flask import (Flask, render_template, jsonify, request)
 import db
-
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
@@ -15,30 +14,7 @@ def serve_rest_index():
 def serve_orders_index():
     return render_template("orders.html", flask_token="Hello   world")
 
-@app.route('/findRestaurant/<restaurant_id>', methods=['GET'])
-def findRestaurant(restaurant_id):
-    conn, cursor = db.db_conn()
-    query = """SELECT r.restaurant_name, r.location, r.rating, r.website, r.food_types
-               FROM Restaurant r
-                WHERE r.restaurant_id = %s"""
-    params = (str(restaurant_id))
-    cursor.execute(query, params)
+from api import orders, restaurant
 
-    restaurant = []
-    for (restaurant_name, location, rating, website, food_types) in cursor:
-        restaurant.append({
-            'location': location,
-            'rating': rating,
-            'website': website,
-            'restaurant_name': restaurant_name,
-            'food_types': food_types
-        })
-    
-    cursor.close()
-    conn.close()
-    return jsonify({'restaurant':restaurant})
-
-
-from api import orders
-
-app.run(host='0.0.0.0',port=8080)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',port=8080)
