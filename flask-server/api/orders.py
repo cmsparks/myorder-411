ec880d3b4cdc2ffef2ba1b3c467a1c51fa81e519
Christian Sparks <csparks1919@hotmail.com>
2021-04-09 00:30:57 -0500

    Refactor and add in better db connection system

---
+++ flask-server/api/orders.py
@@ -0,0 +1,85 @@
+from __main__ import (app)
+import db
+from flask import (jsonify)
+import json
+
+# Get all of a user's order preferences
+@app.route('/user/<user_id>/orders', methods=['GET'])
+def api_user_orders(user_id):
+    print("proc orders on uid: ", user_id)
+    conn, cursor = db.db_conn()
+    orders = []
+    query = """SELECT m.item_name, o.restaurant_id, m.item_price, m.item_desc, r.restaurant_name
+               FROM OrderPreferences o
+                 JOIN Restaurant r ON o.restaurant_id = r.restaurant_id
+                 JOIN MenuItem m ON (o.item_name = m.item_name
+                                        AND o.restaurant_id = m.restaurant_id)
+                WHERE o.user_id = %s"""
+    params = (str(user_id), )
+    cursor.execute(query, params)
+    for (item_name, restaurant_id, item_price, item_desc, restaurant_name) in cursor:
+        orders.append({
+            'item_name': item_name,
+            'restaurant_id': restaurant_id,
+            'item_price': item_price,
+            'item_desc': item_desc,
+            'restaurant_name': restaurant_name
+        })
+
+    cursor.close()
+    conn.close()
+    return jsonify({'orders':orders})
+
+# Update a single order preference
+@app.route('/user/<user_id>/orders/<restaurant_id>+<item_name>', methods=['POST'])
+def api_update_order(user_id):
+    print("proc orders on uid: ", user_id)
+    conn, cursor = db.db_conn()
+    orders = []
+    query = """SELECT m.item_name, m.item_price, m.item_desc, r.restaurant_name
+               FROM OrderPreferences o
+                 JOIN Restaurant r ON o.restaurant_id = r.restaurant_id
+                 JOIN MenuItem m ON (o.item_name = m.item_name
+                                        AND o.restaurant_id = m.restaurant_id)
+                WHERE o.user_id = %s"""
+    params = (str(user_id), )
+    cursor.execute(query, params)
+    for (item_name, item_price, item_desc, restaurant_name) in cursor:
+        orders.append({
+            'item_name': item_name,
+            'item_price': item_price,
+            'item_desc': item_desc,
+            'restaurant_name': restaurant_name
+        })
+    conn.commit() 
+    cursor.close()
+    conn.close()
+    return jsonify({'orders':orders})
+
+# Create a new order preference
+@app.route('/user/<user_id>/orders/<restaurant_id>+<item_name>', methods=['CREATE'])
+def api_create_order(user_id, restaurant_id, item_name):
+    conn, cursor = db.db_conn()
+    
+    query = """INSERT INTO OrderPreferences (user_id, restaurant_id, item_name)
+    VALUES (%s, %s, %s)"""
+    params = (user_id, restaurant_id, item_name)
+    cursor.execute(query, params)
+    conn.commit()
+    cursor.close()
+    conn.close()
+    return json.dumps({'success':True}), 200, {'ContentType':'application/json'} 
+
+# Delete a new order preference
+@app.route('/user/<user_id>/orders/<restaurant_id>+<item_name>', methods=['DELETE'])
+def api_delete_order(user_id, restaurant_id, item_name):
+    conn, cursor = db.db_conn()
+    query = """DELETE FROM OrderPreferences 
+        WHERE (user_id = %s AND (restaurant_id = %s AND item_name = %s))"""
+    params = (user_id, restaurant_id, item_name)
+    cursor.execute(query, params)
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+    return json.dumps({'success':True}), 200, {'ContentType':'application/json'} 
--- flask-server/app.py
+++ flask-server/app.py
@@ -1,64 +1,31 @@
 from flask import (Flask, render_template, jsonify)
+import db
 
 app = Flask(__name__)
 
-import mysql.connector
-mydb = mysql.connector.connect(
-    host= "34.67.0.57",
-    user= "root",
-    password= "1234",
-    database = "one_data"
-)
-
-mycursor = mydb.cursor(buffered=True)
- 
 @app.route('/', methods=['GET'])
 def serve_dir_directory_index():
     return render_template("index.html", flask_token="Hello   world")
-
-@app.route('/orders', methods=['GET'])
-def serve_orders_index():
-    return render_template("orders.html", flask_token="Hello   world")
-
-@app.route('/user/<user_id>/orders', methods=['GET'])
-def api_user_orders(user_id):
-    print("proc orders on uid: ", user_id)
-    orders = []
-    query = """SELECT m.item_name, m.item_price, m.item_desc, r.restaurant_name
-               FROM OrderPreferences o
-                 JOIN Restaurant r ON o.restaurant_id = r.restaurant_id
-                 JOIN MenuItem m ON (o.item_name = m.item_name
-                                        AND o.restaurant_id = m.restaurant_id)
-                WHERE o.user_id = %s"""
-    params = (str(user_id), )
-    mycursor.execute(query, params)
-    for (item_name, item_price, item_desc, restaurant_name) in mycursor:
-        orders.append({
-            'item_name': item_name,
-            'item_price': item_price,
-            'item_desc': item_desc,
-            'restaurant_name': restaurant_name
-        })
-    return jsonify({'orders':orders})
-
-@app.route('/apitest', methods=['GET'])
-def api_test():
-    return "This is a test"
 
 @app.route('/restaurant', methods=['GET'])
 def serve_rest_index():
     return render_template("restaurant.html", flask_token="Hello   world")
 
+@app.route('/orders', methods=['GET'])
+def serve_orders_index():
+    return render_template("orders.html", flask_token="Hello   world")
+
 @app.route('/findRestaurant/<restaurant_id>', methods=['GET'])
 def findRestaurant(restaurant_id):
+    conn, cursor = db.db_conn()
     query = """SELECT r.restaurant_name, r.location, r.rating, r.website, r.food_types
                FROM Restaurant r
                 WHERE r.restaurant_id = %s"""
     params = (str(restaurant_id))
-    mycursor.execute(query, params)
+    cursor.execute(query, params)
 
     restaurant = []
-    for (restaurant_name, location, rating, website, food_types) in mycursor:
+    for (restaurant_name, location, rating, website, food_types) in cursor:
         restaurant.append({
             'location': location,
             'rating': rating,
@@ -66,7 +33,12 @@ def findRestaurant(restaurant_id):
             'restaurant_name': restaurant_name,
             'food_types': food_types
         })
+    
+    cursor.close()
+    conn.close()
     return jsonify({'restaurant':restaurant})
 
+
+from api import orders
 
 app.run(host='0.0.0.0',port=8080)
