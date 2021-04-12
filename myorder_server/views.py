d360c822b0e5c154efd53d7eea5b83fd2858e46f
Marcin <mpitera483@gmail.com>
2021-04-12 01:03:55 -0500

    removed flask_token from views and added new view

--- myorder_server/views.py
+++ myorder_server/views.py
@@ -3,23 +3,26 @@ from flask import (render_template)
 
 @app.route('/', methods=['GET'])
 def serve_dir_directory_index():
-    return render_template("index.html", flask_token="Hello   world")
+    return render_template("index.html")
 
 @app.route('/restaurant', methods=['GET'])
 def serve_rest_index():
-    return render_template("restaurant.html", flask_token="Hello   world")
+    return render_template("restaurant.html")
 
 @app.route('/orders', methods=['GET'])
 def serve_orders_index():
-    return render_template("orders.html", flask_token="Hello   world")
+    return render_template("orders.html")
 
 @app.route('/users', methods=['GET'])
 def get_user():
-    return render_template("users.html", flask_token="tok")
+    return render_template("users.html")
 
 @app.route('/stats', methods=['GET'])
 def get_stats():
-    return render_template("stats.html", flask_token = "tok")
+    return render_template("stats.html")
 
+@app.route('/menuitem', methods=['GET'])
+def get_menu():
+    return render_template("menuitem.html")
 
 from myorder_server.api import db, orders, restaurant, stats, user
