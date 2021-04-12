27cad4d16ab05f5f5b0b90ad0396215d133554d4
Thosar <athosa2@sp21-cs240-33.cs.illinois.edu>
2021-04-11 22:29:05 -0500

    finished restaurant part

--- myorder_server/api/restaurant.py
+++ myorder_server/api/restaurant.py
@@ -39,7 +39,64 @@ def createRestaurant():
     print(query);
     mycursor.execute(query, params)
 
-    # restaurant = []
+    conn.commit()
+    db_rel_lock()
+    print("done")
+    return "Done!!"
+
+@app.route('/deleteRestaurant/<restaurant_id>', methods=['POST'])
+def deleteRestaurant(restaurant_id):
+    conn, mycursor = db_acq_lock()
+    query = """DELETE FROM Restaurant
+               WHERE restaurant_id = %s """
+    params = (str(restaurant_id), )
+    print(query);
+    mycursor.execute(query, params)
+
+    conn.commit()
+    db_rel_lock()
+    print("done")
+    return "DELETED!"
+
+@app.route('/editRestaurant', methods=['POST'])
+def editRestaurant():
+    conn, mycursor = db_acq_lock()
+    print("hellooo")
+    data = request.json;
+    print(data)
+    query = """UPDATE Restaurant
+                SET restaurant_name = %s, location = %s, 
+                rating = %s, website = %s, food_types = %s
+                WHERE restaurant_id = %s """
+    params = (str(data['rname']), str(data['rloc']),
+                str(data['rrate']), str(data['rsite']), str(data['rcuis']), str(data['crid']))
+    print(query);
+    mycursor.execute(query, params)
+
+    conn.commit()
+    db_rel_lock()
+    print("done")
+    return "Done!"
+
+@app.route('/advRestaurant', methods=['GET'])
+def advQueryRestaurant():
+    conn, mycursor = db_acq_lock()
+    print("hellooo")
+    # print(data)
+    query = """
+                SELECT f.restaurant_id, Count(*) as c
+                FROM Feedback f
+                JOIN Restaurant r ON r.restaurant_id = f.restaurant_id
+                WHERE r.rating > 3
+                GROUP BY f.restaurant_id
+                ORDER BY r.rating DESC
+            """
+    print(query);
+    mycursor.execute(query)
+
+    restaurants = {
+        "list": []
+    }
     # for (restaurant_name, location, rating, website, food_types) in mycursor:
     #     restaurant.append({
     #         'location': location,
@@ -48,8 +105,17 @@ def createRestaurant():
     #         'restaurant_name': restaurant_name,
     #         'food_types': food_types
     #     })
-    # return jsonify({'restaurant':restaurant})
+    for pair in mycursor:
+        restaurants["list"].append({
+            "rid": pair[0],
+            "rnum": pair[1]
+        })
+        
+    print(restaurants)
+
     conn.commit()
     db_rel_lock()
+
+    return jsonify({'data':restaurants})
     print("done")
-    return "Done!!"
+    return "Done!"
